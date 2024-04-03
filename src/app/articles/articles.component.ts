5d219789c794864fe891dfb5472644e89c105d4a
Verda Muso <gearmobile@gmail.com>
2024-04-03 17:26:23 +0300

    configure articles

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'scully-articles',
   templateUrl: './articles.component.html',
-  styleUrl: './articles.component.scss'
+  styleUrl: './articles.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  private readonly available$ = inject(ScullyRoutesService).available$;
 
+  public posts$: Observable<ScullyRoute[]> | undefined
+
+  ngOnInit(): void {
+    this.posts$ = this.available$
+      .pipe(map((posts: ScullyRoute[]) => posts.filter((post: ScullyRoute) => post.title)));
+  }
 }
