9f9d09cef178541c5a0a26ac7b80e9b439b161dc
Verda Muso <gearmobile@gmail.com>
2024-04-03 16:23:27 +0300

    add core components

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'scully-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterComponent {
+  public currentDate = new Date();
+}
