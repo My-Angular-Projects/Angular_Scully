94f3918e43a0153dda4f1cc8da637752807464fc
Verda Muso <gearmobile@gmail.com>
2024-04-03 17:12:38 +0300

    add scully package

---
+++ scully.my-blog.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "awesome-blog",
+  outDir: './dist/static',
+  routes: {}
+};
